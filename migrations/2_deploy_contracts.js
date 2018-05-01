344826fd6ffde9468177c003f3ad92c46f05cd3c
Karolis Ramanauskas <hello@karolisram.com>
2018-05-01 15:37:40 +0300

    Logic for deploying on Rinkeby.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,19 +2,24 @@ var NCrowdsale = artifacts.require("./NCrowdsale.sol");
 var NToken = artifacts.require("./NToken.sol");
 
 module.exports = function(deployer) {
-  const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1000, endTime = startTime + 3600 * 24 * 30;
-
-  deployer.deploy(NToken, {gas: 3000000});
-  NToken.deployed().then(function (instance) {
-      instance.owner.call().then(function(owner) {
-        deployer.deploy(NCrowdsale,
-          startTime,
-          endTime,
-          owner,
-          instance.address,
-          owner
-        , {gas: 3000000});
+  web3.eth.getBlockNumber(function(err, blockNumber) {
+    web3.eth.getBlock(blockNumber, function(err, block) {
+      const startTime = block.timestamp + 1000, endTime = startTime + 3600 * 24 * 30;
+  
+      deployer.deploy(NToken, {gas: 3000000}).then(function() {
+        NToken.deployed().then(function(instance) {
+          instance.owner.call().then(function(owner) {
+            deployer.deploy(NCrowdsale,
+              startTime,
+              endTime,
+              owner,
+              instance.address,
+              owner
+            , {gas: 3000000});
+          });
       });
+      });
+    })
   });
 };
 
--- truffle.js
+++ truffle.js
@@ -1,9 +1,21 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var keys = require('./keys');
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(
+        keys.mnemonic,
+        'https://rinkeby.infura.io/7Vjiw3k5GuN1jSfQ0Za0'
+      ),
+      network_id: 4,
+      gasPrice: 35000000000,
+      gas: 7400000
+    },
   }
 };
